2d8ee9e24b115ac0f12270fe3f1eb571736c2117
abderrahmane-elrhassani <abdoghessan@example.com>
2023-10-06 10:36:33 -0700

    8. Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -14,7 +14,10 @@ int main(void)
 	/* Loop through the hexadecimal digits '0' to '9' and 'a' to 'f' */
 	for (hex_digit = '0'; hex_digit <= 'f'; hex_digit++)
 	{
-		putchar(hex_digit); /* Print the current hex digit */
+		if (hex_digit <= '9' || (hex_digit >= 'a' && hex_digit <= 'f'))
+		{
+			putchar(hex_digit); /* Print the current hex digit */
+		}
 	}
 
 	putchar('\n'); /* Print a newline character at the end */
