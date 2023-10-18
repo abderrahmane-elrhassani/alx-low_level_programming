edfbe6409de39145bf81dd10c3d32856ff67462e
abderrahmane-elrhassani <abdoghessan@example.com>
2023-10-18 02:59:41 -0700

    5. Always look up

--- 0x06-pointers_arrays_strings/5-main.c
+++ 0x06-pointers_arrays_strings/5-main.c
@@ -1,19 +1,18 @@
-julien@ubuntu:~/0x06$ cat 5-main.c
 #include "main.h"
 #include <stdio.h>
 
 /**
- * main - check the code
+ * main - Check the code
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    char str[] = "Look up!\n";
-    char *ptr;
+	char str[] = "Look up!\n";
+	char *ptr;
 
-    ptr = string_toupper(str);
-    printf("%s", ptr);
-    printf("%s", str);
-    return (0);
+	ptr = string_toupper(str);
+	printf("%s", ptr);
+	printf("%s", str);
+	return (0);
 }
