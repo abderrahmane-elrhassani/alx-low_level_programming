03418072861e782c0c81be40c9ca7fc8ce692518
abderrahmane-elrhassani <abdoghessan@example.com>
2023-10-27 16:39:30 -0700

    4. To infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line arguments
+ *
+ * Return: 0 for success, 1 for error
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int i;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		char *arg = argv[i];
+		int j = 0;
+
+		while (arg[j] != '\0')
+		{
+			if (arg[j] < '0' || arg[j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+			j++;
+		}
+
+		sum += atoi(arg);
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
