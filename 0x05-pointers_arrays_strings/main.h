c100284d9116c90200d34512fcdd883a6c4ef4d7
abderrahmane-elrhassani <abdoghessan@example.com>
2023-10-27 09:31:19 -0700

    0. A library is not a luxury but one of the necessities of life

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
 #include <unistd.h>
 
 int _putchar(char c);
@@ -15,5 +14,6 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 
 #endif
