8a3ab28c30a5cfc99d064ab06a4a2c227ada34af
abderrahmane-elrhassani <abdoghessan@example.com>
2024-05-07 08:00:10 -0700

    9. Interpolation search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -9,5 +9,6 @@ int binary_search_index(int *array, size_t l, size_t r, int value);
 void print_array(int *array, size_t l, size_t r);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
 
 #endif /* _search_algos_ */
