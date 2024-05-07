7a6f4a32c88240fa39b45c6bd7ba40df3cf4dbad
abderrahmane-elrhassani <abdoghessan@example.com>
2024-05-07 08:05:15 -0700

    10. Exponential search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -10,5 +10,7 @@ void print_array(int *array, size_t l, size_t r);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int b_search(int *array, size_t low, size_t high, int value);
 
 #endif /* _search_algos_ */
